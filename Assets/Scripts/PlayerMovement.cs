355dd4116a4a8721e8ce68d6d467b52228945426
Alexander Jurcau <jurcaua@gmail.com>
2020-01-17 00:52:55 -0500

    initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 10f;
+
+    private Rigidbody m_Rigidbody;
+
+    private Vector3 m_Movement;
+
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+    
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize();
+
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * movementSpeed * Time.deltaTime);
+    }
+}
